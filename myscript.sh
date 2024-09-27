6c47a674e9b1f5d029545457370276294e7f37eb
Keeley Sermon <96189777+ksermon@users.noreply.github.com>
2024-09-27 18:54:42 +0800

    new myscript

--- myscript.sh
+++ myscript.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=work
 #SBATCH --account=courses0101
-#SBATCH --time=00:01:00
+#SBATCH --time=00:05:00
 #SBATCH --output=kgs_Project1.out
 #SBATCH --mem=4G
 
@@ -13,4 +13,4 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=spread
 
 # launch OpenMP code
-srun -N 1 -n 1 -c ${OMP_NUM_THREADS} --gres=gpu:1 --gpus-per-task=1 --gpu-bind=closest ./Project1
+srun -N 1 -n 1 -c ${OMP_NUM_THREADS} ./Project1
