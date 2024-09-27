4eb19455461abe5e4d61860f29de2e0182ccd68e
Keeley Sermon <96189777+ksermon@users.noreply.github.com>
2024-09-27 19:12:39 +0800

    maybe this one will do more threads it sure isnt tasks

--- myscript.sh
+++ myscript.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
 #SBATCH --partition=work
 #SBATCH --account=courses0101
 #SBATCH --mem=4G
 #SBATCH --time=00:01:00
 #SBATCH --output=kgs_Project1.out
 
+export OMP_NUM_THREADS=8
+
 cc -o Project1 -fopenmp ./Project1.c
-srun ./Project1
+srun -c 8 ./Project1
