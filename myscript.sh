d8566daef7a3161aa5359d036293143173673087
Keeley Sermon <96189777+ksermon@users.noreply.github.com>
2024-09-27 19:03:12 +0800

    try a different script i guess

--- myscript.sh
+++ myscript.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=kgs_Project1
 #SBATCH --nodes=1
+#SBATCH --ntasks=28
 #SBATCH --partition=work
 #SBATCH --account=courses0101
-#SBATCH --time=00:05:00
-#SBATCH --output=kgs_Project1.out
 #SBATCH --mem=4G
+#SBATCH --time=00:01:00
 
-# set OpenMP environment variables
-export OMP_NUM_THREADS=1
-
-# launch OpenMP code
-srun -N 1 -n 1 -c 8 ./Project1
+cc -o Project1 -fopenmp ./Project1.c
+srun ./Project1
